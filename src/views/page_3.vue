c5d82c35c23b8e23623522de3aec3129137f9c3b
r.skobtsov@p-s.kz <r.skobtsov@p-s.kz>
2023-07-28 12:40:39 +0300

    главная страница
    Главная страница

---
+++ src/views/page_3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h1>Заявление на внеочередной отпуск</h1>
+    <router-link class="button__classic" to="/page_2">Отпуск 1</router-link>
+    <router-link class="button__classic" to="/page_2">Отпуск 2</router-link>
+    <router-link class="button__classic" to="/page_2">Отпуск 3</router-link>
+    <router-link class="button__classic" to="/page_2">Отпуск 4</router-link>
+  </div>
+</template>
+
+<style lang="scss">
+.button__classic {
+  margin-bottom: 18px;
+}
+</style>
+<script setup>
+</script>
