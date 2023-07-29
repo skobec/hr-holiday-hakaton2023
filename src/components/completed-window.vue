bcc88ba263a8baea8461dfdc7f01b88d5779567e
r.skobtsov@p-s.kz <r.skobtsov@p-s.kz>
2023-07-29 08:54:14 +0300

    predrelease

---
+++ src/components/completed-window.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div class="page">
+      <div class="spinner"></div>
+      <span class="text">Пожалуйста, подождите ...</span>
+    </div>
+    <div class="page">
+      <span class="text">Документы успешно подписаны!</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.text {
+  margin-top: 24px;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px; /* 141.176% */
+  letter-spacing: -0.391px;
+}
+.spinner {
+  width: 64px;
+  height: 64px;
+  border: 0.4em solid #43B741;
+  border-right-color: rgba(208, 194, 194, 0.1);
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
